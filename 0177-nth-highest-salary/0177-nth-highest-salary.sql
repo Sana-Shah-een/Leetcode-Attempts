1b87c912b1a359f8c4692141db624da2926cf2f2
Sana Shaheen <150586586+Sana-Shah-een@users.noreply.github.com>
2026-02-12 02:19:13 +0500

    Clarify OFFSET behavior in getNthHighestSalary function
    Added comment to clarify OFFSET behavior in SQL function.

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -1,5 +1,6 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
+  -- OFFSET zero se start hota mean OFFSET 0 = 1st Row , OFFSET 1 = 2nd Row That's why N=N-1
   SET N=N-1;
   RETURN (
         SELECT DISTINCT salary FROM Employee 
