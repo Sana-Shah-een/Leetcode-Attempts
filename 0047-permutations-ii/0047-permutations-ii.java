e8e55c0c58cf04296519e6eae77d6824e542760b
Sana Shaheen <150586586+Sana-Shah-een@users.noreply.github.com>
2026-02-23 00:00:41 +0500

    02-23-2026 0047-permutations-ii Medium .java

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums); // sort to handle duplicates
+        boolean[] used = new boolean[nums.length];
+        backtrack(nums, new ArrayList<>(), result, used);
+        return result;
+    }
+
+    private void backtrack(int[] nums, List<Integer> tempList, List<List<Integer>> result, boolean[] used) {
+        if (tempList.size() == nums.length) {
+            result.add(new ArrayList<>(tempList));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) continue; // already used
+            // skip duplicates
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
+
+            used[i] = true;
+            tempList.add(nums[i]);
+            backtrack(nums, tempList, result, used);
+            tempList.remove(tempList.size() - 1);
+            used[i] = false;
+        }
+    }
+}
